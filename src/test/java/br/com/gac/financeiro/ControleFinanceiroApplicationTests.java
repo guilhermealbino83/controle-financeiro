455aac0e0202afbc1d2723f8bdb0d1e606afa3a6
Guilherme Albino <guilhermealbino83@gmail.com>
2022-01-30 19:22:07 -0300

    Initial commit

---
+++ src/test/java/br/com/gac/financeiro/ControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gac.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
